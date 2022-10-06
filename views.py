dcdb848b3d5a7d9803b5e3e49469911b966401ea
Hannah Berhe <hannahbberhe@gmail.com>
2022-10-06 10:01:34 -0400

    initial commit

---
+++ views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template ,redirect, url_for
+
+views = Blueprint(__name__,"views")
+
+
+
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+    
+
+@views.route("/go_to_home")
+def go_to_home():
+    return redirect(url_for("views.home"))
+
+@views.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@views.route("/portfolio")
+def portfolio():
+    return render_template("index.html")   
